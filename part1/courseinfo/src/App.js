2389689cf5c522d2a8f07ea6d9fade48bce2b4b2
Francisco García <franciscogarciapozo@gmail.com>
2023-06-03 23:09:46 +0200

    Exercise 1.2 done

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,30 +1,12 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
-
-const Content = (props) => {
+const Part = (props) => {
   return (
     props.exercises.map(
-      exercise => <p id={exercise.id}>
-        {exercise.part} 
-        {exercise.exerciseNum}
-      </p>
+      exercise => <p id={exercise.id}> {exercise.part} {exercise.exerciseNum} </p>
       )
   )
 }
 
-const Total = (props) => {
-  let result = 0
-  props.exercises.forEach(exercise => result += exercise.exerciseNum)
-  return (
-    <p>Number of exercises {result}</p>
-  )
-}
-
 const App = () => {
-  const course = 'Half Stack application development'
   const exercises = [
     { id: 1, part: 'Fundamentals of React', exerciseNum: 10 },
     { id: 2, part: 'Using props to pass data', exerciseNum: 7 },
@@ -33,9 +15,7 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content exercises={exercises} />
-      <Total exercises={exercises} />
+      <Part exercises={exercises} />
     </div>
   )
 }
