93fa791b208000d245a55d56425559d3c8c1b62e
Francisco García <franciscogarciapozo@gmail.com>
2023-06-04 17:50:08 +0200

    Exercise done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,12 +11,18 @@ const Button = (props) => (
 )
 
 const Statistics = ({good, neutral, bad}) =>{
+  let all = good + neutral + bad
+  let average = ((good*1) + (bad*-1)) / all
+  let positivePercentage = good / all * 100
   return (
     <div>
       <h1>statistics </h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>percentage {positivePercentage}%</p>
     </div>
   )
 }
