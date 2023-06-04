4f10c42aa9a8096ed77fbd5640632393014f32bd
Francisco García <franciscogarciapozo@gmail.com>
2023-06-04 12:18:28 +0200

    Remove comments

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,24 +1,3 @@
-/*const Part = (props) => {
-  return (
-    props.exercises.map(
-      exercise => <p id={exercise.id}> {exercise.part} {exercise.exerciseNum} </p>
-      )
-  )
-}*/
-/*
-const App = () => {
-  const exercises = [
-    { id: 1, part: 'Fundamentals of React', exerciseNum: 10 },
-    { id: 2, part: 'Using props to pass data', exerciseNum: 7 },
-    { id: 3, part: 'State of a component', exerciseNum: 14 }
-  ]
-
-  return (
-    <div>
-      <Part exercises={exercises} />
-    </div>
-  )
-}*/
 const Header = (props) => {
   return(
     <h1>props.course</h1>
