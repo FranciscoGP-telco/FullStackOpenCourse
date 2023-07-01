bdc394e666c9825d335fdd7d7e5b39eadf90a68e
Francisco García <franciscogarciapozo@gmail.com>
2023-07-01 11:15:15 +0200

    4.5*: helper functions and unit tests, step3 Done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -11,7 +11,18 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (mostLikedBlog, blog) => {
+    let likes = 0
+    likes = mostLikedBlog ? mostLikedBlog.likes : 0
+    return (likes > blog.likes) ? mostLikedBlog : blog
+  }
+
+  return blogs.reduce(reducer, null)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
