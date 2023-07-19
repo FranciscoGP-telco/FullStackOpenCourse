41d78e27707bf106d4750c7c93b418f7c328bb60
Francisco García <franciscogarciapozo@gmail.com>
2023-07-19 18:47:47 +0200

    4.17: bloglist expansion, step5 Done

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,9 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+//const User = require('../models/user')
 
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({})
+    .populate('user')
+
   response.json(blogs)
 })
 
--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -4,12 +4,17 @@ const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
+    console.log(returnedObject)
     delete returnedObject._id
     delete returnedObject.__v
   }
