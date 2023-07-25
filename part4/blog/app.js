ed16f88d3601a0077367ffe9043671824f937fdb
Francisco García <franciscogarciapozo@gmail.com>
2023-07-25 18:03:42 +0200

    4.18: bloglist expansion, step6 Done

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -7,6 +7,7 @@ require('express-async-errors')
 
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 
@@ -18,5 +19,6 @@ app.use(express.json())
 
 app.use('/api/blogs',blogRouter)
 app.use('/api/users',userRouter)
+app.use('/api/login',loginRouter)
 
 module.exports = app
