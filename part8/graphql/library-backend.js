6b1ec52d39f43445859879dbe303d2f02f8a5586
Francisco García <franciscogarciapozo@gmail.com>
2023-10-26 19:15:58 +0200

    8.3: All authors Done

--- part8/graphql/library-backend.js
+++ part8/graphql/library-backend.js
@@ -102,6 +102,7 @@ const typeDefs = `
     name: String!
     born: Int!
     id: ID!
+    bookCount: Int
   }
 
   type Book {
@@ -116,6 +117,7 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -123,7 +125,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter(book => root.name === book.author).length
   }
 }
 
